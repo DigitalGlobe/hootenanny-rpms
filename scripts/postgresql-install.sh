b25508e722c933dfaae39f49b826b067ea7e014b
jordanmurray35 <jordanmurray5@gmail.com>
2024-08-13 09:56:33 -0400

    version

--- scripts/postgresql-install.sh
+++ scripts/postgresql-install.sh
@@ -17,12 +17,12 @@ set -euo pipefail
 
 PG_DOTLESS=$(echo $PG_VERSION | tr -d '.')
 
+echo ${psql --version}
+
 # Install PostgreSQL server and contrib package (for hstore).
 yum -q -y install \
     postgresql$PG_DOTLESS-contrib-13.15 \
     postgresql$PG_DOTLESS-server-13.15
-
-echo ${psql --version}
 
 # Install alternatives for `initdb` and `pg_ctl` so we won't have to
 # muck with $PATH.
